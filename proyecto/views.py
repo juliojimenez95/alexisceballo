86de736164987892fa9615cbfb60d481d98c0699
julio jimenez garcia <juliojimenegarcia@gmail.com>
2020-08-26 20:45:44 -0500

    version1.0

---
+++ proyecto/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.template import Template
+from django.shortcuts import render
+
+#First View
+def home(request):
+    return render ( request , 'index.htm')
